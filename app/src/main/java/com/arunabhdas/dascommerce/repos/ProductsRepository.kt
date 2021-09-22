1623aa529e74e6471fa683b8f1ef98db9636c984
Arunabh Das <arunabhdas@gmail.com>
2021-09-22 14:58:37 -0400

    Improved fetchProducts implementation

--- app/src/main/java/com/arunabhdas/dascommerce/repos/ProductsRepository.kt
+++ app/src/main/java/com/arunabhdas/dascommerce/repos/ProductsRepository.kt
@@ -45,7 +45,7 @@ class ProductsRepository {
 
     fun fetchProducts(): List<Product> {
         val json =
-            URL("https://gist.githubusercontent.com/arunabhdas/df34a33a06e985d85f2ba7f6e635c600/raw/df0aeaea2def4aa1c9b0f266d032b6733c3fbaaa/shopping_products.json").readText()
+            URL(BASE_URL_HTTP).readText()
         return Gson().fromJson(json, Array<Product>::class.java).toList()
     }
 }
