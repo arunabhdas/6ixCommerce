6352f466a3d30c012148ff6f5d63b82478acaad0
Arunabh Das <arunabhdas@gmail.com>
2021-09-22 14:30:09 -0400

    Cleanup ProductDetailsViewModel

--- app/src/main/java/com/arunabhdas/dascommerce/productdetails/ProductDetailsViewModel.kt
+++ app/src/main/java/com/arunabhdas/dascommerce/productdetails/ProductDetailsViewModel.kt
@@ -15,17 +15,6 @@ class ProductDetailsViewModel : ViewModel() {
 
     val productDetails = MutableLiveData<Product>()
 
-    fun fetchProductDetails(productTitle: String) {
-        viewModelScope.launch(Dispatchers.Default) {
-            val json =
-                URL("https://gist.githubusercontent.com/arunabhdas/df34a33a06e985d85f2ba7f6e635c600/raw/df0aeaea2def4aa1c9b0f266d032b6733c3fbaaa/shopping_products.json").readText()
-            val list = Gson().fromJson(json, Array<Product>::class.java).toList()
-            val product = list.first { it.title == productTitle }
-
-            productDetails.postValue(product)
-        }
-    }
-
     fun fetchAllProductDetails(productTitle: String) {
         viewModelScope.launch(Dispatchers.Default) {
             val json =
